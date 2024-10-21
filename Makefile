eea7ec78f41ef5ab48751fda1384c50b2941d7c9
LouisPsn <lpierson002@enseirb-matmeca.fr>
2024-10-21 17:45:04 +0200

    Bluetooth fonctionel

--- Makefile
+++ Makefile
@@ -4,3 +4,8 @@ install :
 main : src/main.c
 	avr-gcc -mmcu=atmega328p -DF_CPU=13000000 -Os -o build/main.elf src/main.c
 	avr-objcopy -O binary build/main.elf build/main.bin
+
+
+bluetooth: src/bluetooth.c
+	avr-gcc -mmcu=atmega328p -DF_CPU=13000000 -Os -o build/main.elf src/bluetooth.c
+	avr-objcopy -O binary build/main.elf build/main.bin
---
+++ src/bluetooth.c
@@ -0,0 +1,55 @@
+/*
+Communication par bluetooth
+*/
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define FOSC 13000000 // Clock Speed
+#define BAUD 38400
+#define MYUBRR FOSC / 16 / BAUD - 1
+
+void USART_Init(unsigned int ubrr)
+{
+    /*Set baud rate */
+    UBRR0H = (unsigned char)(ubrr >> 8);
+    UBRR0L = (unsigned char)ubrr;
+    /* Enable receiver and transmitter */
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+    /* Set frame format: 8data, 2stop bit */
+    UCSR0C = (1 << USBS0) | (3 << UCSZ00);
+}
+
+unsigned char USART_Receive(void)
+{
+    /* Wait for data to be received */
+    while (!(UCSR0A & (1 << RXC0)))
+        ;
+    /* Get and return received data from buffer */
+    return UDR0;
+}
+
+void USART_Transmit(unsigned char data)
+{
+    /* Wait for empty transmit buffer */
+    while (!(UCSR0A & (1 << UDRE0)))
+        ;
+    /* Put data into buffer, sends the data */
+    UDR0 = data;
+}
+
+void transmit_txt(unsigned char* txt, int lenght)
+{
+    for(int i; i < lenght; i++) {
+        USART_Transmit(txt[i]);
+    }
+}
+
+void main(void)
+{
+    USART_Init(MYUBRR);
+    while (1)
+    {
+        USART_Transmit(USART_Receive());
+    }
+}
