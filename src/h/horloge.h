a1a9f9635e73a345382bc899ada42894d628ec9d
Gouhey Estebane <estebane.gouhey@gmail.com>
2024-12-04 21:06:30 +0100

    necessary change for main

--- src/h/horloge.h
+++ src/h/horloge.h
@@ -10,6 +10,7 @@
 #include "bluetooth.h"
 #include "../h/clock.h"
 
+/*
 volatile static int16_t tic_par_tour = 0;
 volatile static int16_t tic = 0;
 volatile static char first = 1;
@@ -17,7 +18,7 @@ volatile static char first = 1;
 volatile static int8_t heures = 10;
 volatile static int8_t min = 10;
 volatile static int8_t sec = 0;
-
+*/
 void horloge_trigo();
 
 void horloge_2();
